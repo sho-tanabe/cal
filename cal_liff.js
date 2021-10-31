329f578e9c37640413e4ce5cf52b15d491ceaf35
sho-tanabe <93310029+sho-tanabe@users.noreply.github.com>
2021-10-31 10:03:17 +0900

    Update cal_liff.js

--- cal_liff.js
+++ cal_liff.js
@@ -72,3 +72,60 @@ function sendautomail(text){
         message => alert(message)
     )
 };
+
+
+
+
+
+//----------------------------------------以下電卓テスト----------------------------------------
+const textbox1 = document.getElementById("textbox1")
+const textbox2 = document.getElementById("textbox2")
+const type = document.getElementById("type")
+const display = document.getElementById("answer")
+
+let secondNum = 0;
+let ans = ''
+let kigou = "";
+let isInit = true;
+
+function sign(btn) {
+    kigou = btn;
+    type.innerHTML = btn;
+}
+
+function run() {
+    if (isInit) {
+        ans = Number(textbox1.value)
+    }
+    secondNum = Number(textbox2.value)
+    switch (kigou) {
+        case "+":
+            ans += secondNum;
+            break;
+        case "-":
+            ans -= secondNum;
+            break;
+        case "×":
+            ans *= secondNum;
+            break;
+        case "÷":
+            ans /= secondNum;
+            break;
+        default:
+            ans = 0;
+    }
+    display.innerHTML = ans;
+    isInit = false;
+}
+
+function refresh() {
+    isInit = true;
+    secondNum = 0;
+    ans = 0;
+    kigou = "";
+    type.innerHTML = "";
+    display.innerHTML = 0;
+    textbox1.value = "";
+    textbox2.value = "";
+}
+//----------------------------------------以上電卓テスト----------------------------------------
